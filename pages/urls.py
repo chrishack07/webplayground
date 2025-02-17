7c5343bd66241ca36d111464625dd417b3039533
christopher.acuna <christopher.acuna@bbva.com>
2025-02-16 23:54:12 -0500

    feat(*): Develop CBV UpdateView

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import PageListView, PageDetailView, PageCreateView
+from .views import PageListView, PageDetailView, PageCreateView, PageUpdateView
 
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
     path('create/', PageCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', PageUpdateView.as_view(), name='update'),
 ], 'pages'); # This is a tuple with the URL patterns and the app name
